d92dd246ac97b414cec16ad3c78ec7a7ccc5380d
akhil pune <monsterboy67@gmail.com>
2022-10-26 12:40:33 +0530

    changes: changes to consensus params

--- types/params_test.go
+++ types/params_test.go
@@ -118,7 +118,7 @@ func TestConsensusParamsUpdate(t *testing.T) {
 		{
 			makeParams(1, 2, 10, 3, 0, valEd25519),
 			&abci.ConsensusParams{
-				Block: &abci.BlockParams{
+				Block: &tmproto.BlockParams{
 					MaxBytes: 100,
 					MaxGas:   200,
 				},
--- types/protobuf.go
+++ types/protobuf.go
@@ -99,9 +99,10 @@ func (tm2pb) ValidatorUpdates(vals *ValidatorSet) []abci.ValidatorUpdate {
 
 func (tm2pb) ConsensusParams(params *tmproto.ConsensusParams) *abci.ConsensusParams {
 	return &abci.ConsensusParams{
-		Block: &abci.BlockParams{
+		Block: &tmproto.BlockParams{
 			MaxBytes: params.Block.MaxBytes,
 			MaxGas:   params.Block.MaxGas,
+			TimeIotaMs: params.Block.TimeIotaMs,
 		},
 		Evidence:  &params.Evidence,
 		Validator: &params.Validator,
